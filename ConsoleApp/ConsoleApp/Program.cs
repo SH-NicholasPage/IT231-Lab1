d937b3a2c0024121c31e39b7684630ef116f252f
Nicholas Page <npage@southhills.edu>
2023-11-28 20:19:24 -0500

    multiplication

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,6 +6,12 @@
         {
             int x = Int32.Parse(Console.ReadLine()!);
             int y = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine($"Multiplication: {Multiplication(x, y)}");
+        }
+
+        public static int Multiplication(int x, int y)
+        {
+            return x * y;
         }
     }
 }
